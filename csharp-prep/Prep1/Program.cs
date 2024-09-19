2c9d657f4e1499791b14569ac84b1d16148ee89c
TyroneMartin <tyrone306@byui.edu>
2024-09-19 13:26:30 -0600

    Prep 1 assignment completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Practice assignment
+        // Get user's first and last name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine("");
+        // Print user's full name
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+           Console.WriteLine("This is in C#.");
+
+        int addNum;
+        int number = 42;
+        addNum = number + 6;
+        Console.WriteLine(addNum);
+
+        string color = "blue";
+        Console.WriteLine("The color is " + color);
+        // string user_color;
+        Console.Write("What is your favorite color? ");
+        string user_color = Console.ReadLine();
+        Console.WriteLine("Your favorite color is " + user_color);
+        Console.WriteLine($"I like that color too. My favorite color is {color} and your favorite color is {user_color}");
+
     }
 }
