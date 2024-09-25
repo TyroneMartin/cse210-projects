0a148e83fb83392b2186e10aa064526496313269
TyroneMartin <tyrone306@byui.edu>
2024-09-25 10:19:33 -0600

    core assignment completed 1-3 for using list and adding items to the list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // using System.Collections.Generic;
+
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        Console.Write("");
+        Console.Write("Enter number: ");
+        float number = float.Parse(Console.ReadLine());
+  
+        // Console.Write("Enter number: ");
+
+        
+        // Store user numbers in a list of floategers
+        List<float> numbers = new List<float>();
+
+
+        // float total.sum(numbers);
+        float total = 0;  
+        float average = 0;
+        float max = 0;
+
+
+        while (number != 0)
+        {
+            numbers.Add(number);
+            total += number;
+
+            average = total / numbers.Count;
+            max = numbers.Max();
+
+            Console.Write("Enter number: ");
+            number = float.Parse(Console.ReadLine());
+        }
+
+     
+        Console.WriteLine($"The total is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine($"The list numbers are: {numbers.Count}");
+
     }
 }
