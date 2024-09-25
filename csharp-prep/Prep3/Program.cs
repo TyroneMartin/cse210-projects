b0f1651b2d291dca0e5945b7abc51df837b4f5ec
TyroneMartin <tyrone306@byui.edu>
2024-09-25 00:44:36 -0600

    week 03 random number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        
+        // Intro to user
+        Console.WriteLine("Guess a random number from 1-100");
+        Console.WriteLine("");
+
+        // Console.Write("What is the magic number? ");
+        // string userRandomNumber = Console.ReadLine();
+
+        // Get random number from 1-100
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 100);
+
+       do 
+       {
+       // get user input
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+
+        // Console.WriteLine($"The Random number is {randomNumber}"); 
+
+        // convert user input to int
+        int userNumber = int.Parse(userGuess);
+        // convert user random number from string to int
+        // int systemNumber = int.Parse(userRandomNumber);
+
+        // check if guess is higher or lower than random number
+        if (randomNumber > userNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (randomNumber < userNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it! The magic number was " + randomNumber);
+            Console.WriteLine("");
+            Console.Write($"Do you want to play again (yes or no)?  ");
+            string playAgain = Console.ReadLine();
+            if (playAgain == "yes")
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("Thank you for playing! Goodbye!");
+                break;
+            }   
+        }
+
+       } while (true);       
     }
+
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -6,9 +6,6 @@ class Program
     {
         // Console.WriteLine("Hello Learning03 World!");
 
-        //   What is the magic number? 6
-        //   What is your guess? 4
-        //   Higher
 
 
         
