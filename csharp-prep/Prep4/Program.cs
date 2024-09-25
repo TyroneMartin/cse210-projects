1443c0dee9c01aefe79a61c983a546c8d2d25ccb
TyroneMartin <tyrone306@byui.edu>
2024-09-25 10:37:57 -0600

    Need to fix smallestPositive logic

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -8,42 +8,49 @@ class Program
     {
         // using System.Collections.Generic;
 
-
         Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
         Console.Write("");
         Console.Write("Enter number: ");
         float number = float.Parse(Console.ReadLine());
   
-        // Console.Write("Enter number: ");
-
         
-        // Store user numbers in a list of floategers
+        // Store user numbers in a list of float numbers
         List<float> numbers = new List<float>();
 
 
-        // float total.sum(numbers);
-        float total = 0;  
+        // Initialize variables
+        float sum = 0;  
         float average = 0;
         float max = 0;
+        float smallestPositive = 0;
 
 
         while (number != 0)
         {
             numbers.Add(number);
-            total += number;
+            sum += number;
 
-            average = total / numbers.Count;
+            average = sum / numbers.Count;
             max = numbers.Max();
+
 
             Console.Write("Enter number: ");
             number = float.Parse(Console.ReadLine());
         }
 
-     
-        Console.WriteLine($"The total is: {total}");
+        // smallestPositive = numbers.Find(x => x > 0);
+
+        smallestPositive = numbers.Find(numbers => numbers > 0);
+
+
+    // Need to fix smallestPositive logic
+
+
+        Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average is: {average}");
         Console.WriteLine($"The largest number is: {max}");
         Console.WriteLine($"The list numbers are: {numbers.Count}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
 
     }
 }
